2f7b39c572c45169fa10a9c571bba9cf5f869254
Marc López <marc.lopezroca@gmail.com>
2019-01-03 20:03:06 +0100

    feat(folderListener): Extract method

--- rpisec/FolderListener.py
+++ rpisec/FolderListener.py
@@ -26,14 +26,29 @@ class SendCreatedImage(PatternMatchingEventHandler):
 class FolderListener(Thread):
     def __init__(self, bot, configuration):
         self.path = configuration.camera_save_path
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
+        self.create_folder(self.path)
 
         self.changed = Signal()
         self.changed.connect(bot.send_message)
         self.event_handler = SendCreatedImage("[*.jpg]", self.changed)
         super().__init__()
         self.daemon = True
+
+    @staticmethod
+    def create_folder(path):
+        if not os.path.exists(path):
+            try:
+                os.makedirs(path)
+            except PermissionError as e:
+                logger.error(
+                    "create_folder doesn't have permissions in {0}, failed with error {1}".format(repr(path), repr(e)))
+                raise Exception('Failed to create the images folder with error: {0}'.format(repr(e)))
+            except Exception as e:
+                logger.error(
+                    "create_folder failed with error {1}".format(repr(path), repr(e)))
+                raise Exception('Failed to create the images folder with error: {0}'.format(repr(e)))
+
+        pass
 
     def run(self):
         observer = Observer()
@@ -48,4 +63,5 @@ class FolderListener(Thread):
         except Exception as e:
             logger.error('FolderListener has a crash: {0}'.format(repr(e)))
             observer.stop()
+            raise Exception('FolderListener has a crash: {0}'.format(repr(e)))
         observer.join()
